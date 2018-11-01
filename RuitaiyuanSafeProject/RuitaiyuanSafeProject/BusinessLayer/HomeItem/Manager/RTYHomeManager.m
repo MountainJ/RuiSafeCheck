c2a4c1d56f17e926980bc271e1f7daafb336c55a
mountainJay <cqzl55@163.com>
2018-11-01 16:46:33 +0800

    modify name , add manager

---
+++ RuitaiyuanSafeProject/RuitaiyuanSafeProject/BusinessLayer/HomeItem/Manager/RTYHomeManager.m
@@ -0,0 +1,13 @@
+//
+//  RTYHomeManager.m
+//  RuitaiyuanSafeProject
+//
+//  Created by 张毅 on 2018/11/1.
+//  Copyright © 2018年 MountainJay. All rights reserved.
+//
+
+#import "RTYHomeManager.h"
+
+@implementation RTYHomeManager
+
+@end
